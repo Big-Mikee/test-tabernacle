7b538d1bd09e51a6c21d532ebd1cfc8b2d6d6ac3
Big Mike <michaelboiz86@gmail.com>
2025-11-20 12:43:54 +0100

    fb tables

---
+++ Models/FB/FormField.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test_tabernacle.Models.FB
+{
+    [Table("fb_form_fields")]
+    public class FormField : AuditEntity
+    {
+        [MaxLength(50)]
+        public string FormTemplateCode { get; set; }
+
+        [MaxLength(50)]
+        public string ParentFieldCode { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        public string DataType { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        [MaxLength(100)]
+        public string GroupName { get; set; }
+
+        public int SortOrder { get; set; }
+    }
+}
--- Models/FB/FormTemplateHistory.cs
+++ Models/FB/FormTemplateHistory.cs
@@ -6,29 +6,22 @@ namespace test_tabernacle.Models.FB
     [Table("fb_form_template_history")]
     public class FormTemplateHistory : AuditEntity
     {
-        [Column("TemplateCode")]
         [StringLength(50)]
         public string TemplateCode { get; set; }
 
-        [Column("Version")]
         public int Version { get; set; }
 
-        [Column("Name")]
         [StringLength(100)]
         public string Name { get; set; }
 
-        [Column("Module")]
         [StringLength(50)]
         public string Module { get; set; }
 
-        [Column("JsonDefinition")]
         public string JsonDefinition { get; set; }
 
-        [Column("ChangedBy")]
         [StringLength(50)]
         public string ChangedBy { get; set; }
 
-        [Column("ChangedOn")]
         public DateTime ChangedOn { get; set; }
     }
 }
