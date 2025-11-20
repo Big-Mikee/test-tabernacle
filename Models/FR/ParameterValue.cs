499aeaf8bfc265545760f7f50c5c723551a87231
Memijo Olaoluwa <molaoluwa@lagetronix.com>
2025-11-20 12:33:44 +0100

    parameter value added to fr

---
+++ Models/FR/ParameterValue.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test_tabernacle.Models.FR
+{
+    public class ParameterValue : AuditEntity
+    {
+        [StringLength(50)]
+        public string ParameterCode { get; set; }
+
+        public string Value { get; set; }
+
+        [StringLength(50)]
+        public string UserCode { get; set; }
+
+        [StringLength(50)]
+        public string HierarchyInstanceCode { get; set; }
+
+    }
+}
