2a9cf997c0f6ab74dd1262d3c04049b38583815d
Memijo Olaoluwa <molaoluwa@lagetronix.com>
2025-11-20 13:01:05 +0100

    added tables to officer assignments

---
+++ Models/OA/Assignment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test_tabernacle.Models.OA
+{
+    [Table("oa_assignments")]
+    public class Assignment : AuditEntity
+    {
+        [Required]
+        [MaxLength(50)]
+        public string UserCode { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string OfficeCode { get; set; }
+
+        [MaxLength(50)]
+        public string HierarchyInstanceCode { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+    }
+
+}
