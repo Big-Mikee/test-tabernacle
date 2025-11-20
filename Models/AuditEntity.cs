55e0c9f59b3f2b8af57c793bd358aeaf8ba791d7
Big Mike <michaelboiz86@gmail.com>
2025-11-20 10:53:05 +0100

    organization table

---
+++ Models/AuditEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test_tabernacle.Models
+{
+    public class AuditEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Key]
+        [MaxLength(50)]
+        public string Code { get; set; }
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        [MaxLength(50)]
+        public string CreatedBy { get; set; }
+
+        public DateTime? ModifiedOn { get; set; } = DateTime.UtcNow;
+
+        [MaxLength(50)]
+        public string? ModifiedBy { get; set; }
+    }
+}
