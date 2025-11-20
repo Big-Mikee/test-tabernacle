bfd5175da78244306d6217ed667afc1e146a12de
Big Mike <michaelboiz86@gmail.com>
2025-11-20 11:34:23 +0100

    heirarchy level fields

---
+++ Models/SO/HierarchyLevelField.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test_tabernacle.Models.SO
+{
+    [Table("so_hierarchy_level_fields")]
+    public class HierarchyLevelField : AuditEntity
+    {
+
+        [MaxLength(50)]
+        public string HierarchyLevelCode { get; set; }
+
+        [MaxLength(100)]
+        public string FieldName { get; set; }
+
+        public bool IsRequired { get; set; }
+
+        public int SortOrder { get; set; }
+    }
+}
