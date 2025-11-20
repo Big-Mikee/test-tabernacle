0a5a0862ee5e0ab0199cca637117259545eb9101
Memijo Olaoluwa <molaoluwa@lagetronix.com>
2025-11-20 11:56:45 +0100

    Added ac_audit logs

---
+++ Models/AC/AcAuditLogs.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace test_tabernacle.Models.AC
+{
+    [Table("ac_auditLogs")]
+    public class AcAuditLogs : AuditEntity
+    {
+        [StringLength(50)]
+        public string UserCode { get; set; }
+
+        [StringLength(100)]
+        public string Action { get; set; }
+
+        [StringLength(50)]
+        public string EntityType { get; set; }
+
+        [StringLength(50)]
+        public string EntityCode { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        [StringLength(45)]
+        public string IPAddress { get; set; }
+
+        [StringLength(500)]
+        public string DeviceInfo { get; set; }
+    }
+}
